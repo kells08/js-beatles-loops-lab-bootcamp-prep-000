81f2e3318f86d4daa6a0cef7458066fb63f77d8f
Kelly Lwakatare <devkells00@gmail.com>
2018-05-25 05:37:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function theBeatlesPlay(musicians, instruments) {
+  var play = []
+  
+  for (var i = 0, l = musicians.length; i < l; i++) {
+    play.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+  return play;
+}
+
+function johnLennonFacts(facts) {
+  var shout = []
+  
+  var i = 0;
+  while(i < facts.length) {
+    shout.push(`${facts[i]}!!!`)
+  }
+  
+  return shout;
+}
